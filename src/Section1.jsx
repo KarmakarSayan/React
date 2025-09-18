590f659286a75697020add0638d94f5f7d99de69
KarmakarSayan <sayan18karmakar@gmail.com>
2025-09-19 00:18:25 +0530

    day-1

---
+++ src/Section1.jsx
@@ -0,0 +1,16 @@
+import "./Design.css";
+
+function Sec1() {
+    return (
+        <>
+            <div className="sectionn1">
+                <img src="/1000226828.jpg" alt="Detail" />
+                <div className="details">
+                    <p>Sayan Karmakar <br />Sayan18karmakar@gmai.com <br />+91-769869826</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Sec1;
